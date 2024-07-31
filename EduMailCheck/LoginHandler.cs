6715519a960eafdbf56c3a533866edabff285f43
Cansu Ökemen <cansuokemen@gmail.com>
2024-07-31 16:36:14 +0300

    classes were placed in separate files

---
+++ EduMailCheck/LoginHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EduMailCheck
+{
+    public class LoginHandler
+    {
+        public static void Display()
+        {
+            Console.WriteLine("E-posta giriniz:");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Şifrenizi giriniz:");
+            string password = Console.ReadLine();
+
+            Console.WriteLine("Girdiğiniz E-posta: " + email);
+            Console.WriteLine("Girdiğiniz Şifre: " + password);
+        }
+    }
+}
---
+++ EduMailCheck/PasswordChecker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduMailCheck
+{
+    public class PasswordChecker
+    {
+        public static bool CheckPasswords(string password1)
+        {
+            Console.WriteLine("Şifrenizi yeniden giriniz:");
+            string password2 = Console.ReadLine();
+
+            while (password1 != password2)
+            {
+                Console.WriteLine("Şifreler uyuşmuyor. Lütfen aynı şifreyi giriniz.");
+                Console.WriteLine("Şifrenizi giriniz:");
+                password1 = Console.ReadLine();
+                Console.WriteLine("Şifrenizi yeniden giriniz:");
+                password2 = Console.ReadLine();
+            }
+
+            return true;
+        }
+    }
+}
+
